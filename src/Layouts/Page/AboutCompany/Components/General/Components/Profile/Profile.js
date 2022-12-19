288952dd1228f41bf89a9f538f7ea22bfaaa3ae3
elina4576 <120501080+elina4576@users.noreply.github.com>
2022-12-19 12:46:28 +0000

    обновила паддинги и обводку

--- src/Layouts/Page/AboutCompany/Components/General/Components/Profile/Profile.js
+++ src/Layouts/Page/AboutCompany/Components/General/Components/Profile/Profile.js
@@ -3,8 +3,8 @@ function Profile() {
     return (
         <div className='d-inline-flex flex-row'>
             <Avatar/>
-            <div className='ms-3 d-inline-flex flex-column justify-content-center'>
-                <div className='title'>SPBEAUTY</div>
+            <div className='ms-4 d-inline-flex flex-column justify-content-center'>
+                <div className='mb-2 title'>SPBEAUTY</div>
                 <div className="mb-1">Салон красоты</div>
                 <div>Санкт-Петербург, Невский пр-т. 28</div>
             </div>                  
